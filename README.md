f76bb18a5ec7f6996915ebc81db7518326f0db2a
one-for-all <jianan.li@yahoo.com>
2017-10-14 18:33:26 -0700

    script for adding skill list

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 1. Create python3 virtual environment
 2. Activate the virtual environment
 3. Install packages using requirements.txt
-3. Inside team_builder dir,
+4. Change directory to team_builder
+5. python manage.py migrate
---
+++ team_builder/scripts/data_import.py
@@ -0,0 +1,17 @@
+import os
+import django
+import sys
+import json
+
+
+sys.path.append('../team_builder')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team_builder.settings")
+django.setup()
+
+from accounts.models import Skill
+
+
+with open('pugorugh/scripts/skills.json', 'r') as file:
+    data = json.load(file)
+    for skill in data:
+        Skill.objects.create(name=skill)
